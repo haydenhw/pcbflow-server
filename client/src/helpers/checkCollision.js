7e302f86cbfed4cc8e2132dad046939f0b6e9569
hayden7913 <hayden321@gmail.com>
2017-04-21 23:37:23 -0700

    unwanted nodes in cd function

--- client/src/helpers/checkCollision.js
+++ client/src/helpers/checkCollision.js
@@ -1,15 +1,18 @@
-
-
-
 export default function checkCollision(nodeArray, callback) {
   let collidingNodes = [];
-  
+  console.log(nodeArray)
   nodeArray.forEach((node) => {
     const nodeBox = node.attrs ? node.attrs : node;
     // console.log(nodeBox.width, nodeBox.height)
+    //console.log(nodeBox.x, nodeBox.y, nodeBox.width, nodeBox.height)
     const nodeBoxWidth = callback ? callback(node, nodeBox.width) : nodeBox.width;
-    const nodeBoxHeight = callback ? callback(node, nodeBox.height) : nodeBox.height;
+    const nodeBoxHeight = false ? callback(node, nodeBox.height) : nodeBox.height;
     // console.log(nodeBoxWidth, nodeBoxHeight)
+  /*  const halfDiff = (nodeBox.height-nodeBox.width)/2;
+    const nodeLeft = nodeBox.x - halfDiff;
+    const nodeRight = nodeLeft + nodeBox.height;
+    const nodeTop = nodeBox.y - halfDiff;
+    const nodeBottom = nodeTop + nodeBox.width;*/
     const nodeLeft = nodeBox.x;
     const nodeRight = nodeBox.x + nodeBoxWidth;
     const nodeTop = nodeBox.y;
@@ -18,16 +21,36 @@ export default function checkCollision(nodeArray, callback) {
     nodeArray.forEach((otherNode) => {
        const otherBox = otherNode.attrs ? otherNode.attrs : otherNode;
         if (nodeBox !== otherBox) {
-          const otherBoxWidth = callback ? callback(node, nodeBox.width) : otherBox.width;
-          const otherBoxHeight = callback ? callback(node, nodeBox.height) : otherBox.height;
-          const otherLeft = otherBox.x;
+          const otherBoxWidth = callback ? callback(node, otherBox.width) : otherBox.width;
+          const otherBoxHeight = false ? callback(node, otherBox.height) : otherBox.height;
+          
+          const halfDiff = (otherBox.height-otherBox.width)/2;
+          const otherLeft = otherBox.x - halfDiff;
+          const otherRight = otherLeft + otherBox.height;
+          const otherTop = otherBox.y - halfDiff;
+          const otherBottom = otherTop + otherBox.width;
+          
+          console.log(//nodeLeft,
+                      //nodeRight,
+                    //  "node top",
+                    //nodeTop,
+                      //nodeBottom
+                    )
+          console.log(//otherLeft,
+                      //otherRight,
+                      //"other bottom",
+                      //otherTop,
+                      //otherBottom)
+        /*const otherLeft = otherBox.x;
           const otherRight = otherBox.x + otherBoxWidth;
           const otherTop = otherBox.y;
-          const otherBottom = otherBox.y + otherBoxHeight;
+          const otherBottom = otherBox.y + otherBoxHeight;*/
+        )
+         
          
           const collideHoriz = nodeLeft < otherRight && nodeRight > otherLeft;
           const collideVert = nodeTop < otherBottom && nodeBottom > otherTop;
-
+          console.log(nodeTop, otherBottom, node, otherNode)
           if (collideHoriz && collideVert) {
           
             collidingNodes.push(otherNode);
@@ -40,6 +63,6 @@ export default function checkCollision(nodeArray, callback) {
        }
      });
   });
-  console.log('colliding nodes' , collidingNodes)
+//  console.log('colliding nodes' , collidingNodes)
   return collidingNodes;
 }
--- client/src/helpers/enforceRules.js
+++ client/src/helpers/enforceRules.js
@@ -5,17 +5,19 @@ Array.prototype.diff = function(a) {
     return this.filter(function(i) {return a.indexOf(i) < 0;});
 };
 
-function swapDimensions(node, dimension){
+function adjustDimesionForsRotation(node, dimension){
   //console.log(node.get('.innerGroup')[0].attrs.rotation)
   /*const rotation = node.get('.innerGroup')[0].attrs.rotation
   if (rotation === 90 || rotation === 270) {
-    return dimension === node.width ? node.length : node.width; 
+    const { width, height } = node.attrs;
+    const halfDiff = (height-width)/2;
+    return 2 * height -  node.attrs.x; 
   }*/
   return dimension; 
 }
 
 export default function enforceRules(nodeArray, perimeterNode, ruleBreakingAction, ruleFollowingAction) {
-  const collidingNodes = checkCollision(nodeArray, swapDimensions);
+  const collidingNodes = checkCollision(nodeArray,adjustDimesionForsRotation);
   const outOfBoundsNodes = checkExceedsPerimter(nodeArray, perimeterNode);
   const ruleBreakingNodes = [...collidingNodes, ...outOfBoundsNodes]
   const ruleFollowingNodes = nodeArray.diff(ruleBreakingNodes);
