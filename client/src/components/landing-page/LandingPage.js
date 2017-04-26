c22273047e5ac7c573b301aeedad24d561a935f7
hayden7913 <hayden321@gmail.com>
2017-04-26 16:03:18 -0700

    import React from 'react'; import './landing-page-styles/LandingPage.css'
    export default function LandingPage(props) {
      const imgUrl ='images/landing-background.jpg'
      const backgroundStyle = {
        backgroundImage: `url(${imgUrl})`
      }
      return (
        <div>
          <div className="landing-navbar">
            <img className="white-logo-image" src="images/white-logo.png" alt=""/>
            <span  className="landing-logo-text" >PCB Design</span>
          </div>
            <div style={backgroundStyle} className="landing-page-content">
              <div className="landing-page-text">
                <div>Design Production Ready Printed Circuit Boards</div>
                <div>in Record Time</div>
                <div className="landing-page-button">Get Started</div>
              </div>
            </div>
        </div>
      );
    }

--- client/src/components/landing-page/LandingPage.js
+++ client/src/components/landing-page/LandingPage.js
@@ -2,19 +2,24 @@ import React from 'react';
 import './landing-page-styles/LandingPage.css'
 
 export default function LandingPage(props) {
+  const imgUrl ='images/landing-background.jpg'
+  const backgroundStyle = {
+    backgroundImage: `url(${imgUrl})`
+  }
   return (
     <div>
       <div className="landing-navbar">
         <img className="white-logo-image" src="images/white-logo.png" alt=""/>
         <span  className="landing-logo-text" >PCB Design</span>
       </div>
-        <div className="landing-page-body">
-          <img className="landing-page-background-image" src="./images/landing-background.jpg" alt="landing page background"/>    
-          <div className="text">
-            <div>This is some text</div>
-            <div>This is some more text</div>
+        <div style={backgroundStyle} className="landing-page-content">
+          <div className="landing-page-text">
+            <div>Design Production Ready Printed Circuit Boards</div>
+            <div>in Record Time</div>
+            <div className="landing-page-button">Get Started</div>
           </div>
         </div>
     </div>
   );
 }
+
