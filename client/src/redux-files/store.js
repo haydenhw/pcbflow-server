99ac4a041f097b0fc7bb4b7959ac7751d97838cb
hayden7913 <hayden321@gmail.com>
2017-04-28 16:43:38 -0700

    build

--- client/src/redux-files/store.js
+++ client/src/redux-files/store.js
@@ -7,5 +7,6 @@ import * as actions from 'actions/indexActions';
 
 export default createStore(
   rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk /* logger()*/),
 );
