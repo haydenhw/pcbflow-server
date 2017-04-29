7deed63f17635a2a4f014846b825bb8bb7539be8
hayden7913 <hayden321@gmail.com>
2017-04-29 10:35:28 -0700

    fixed conflict

--- client/src/redux-files/store.js
+++ client/src/redux-files/store.js
@@ -1,25 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
-<<<<<<< HEAD
-//import thunk from 'redux-thunk';
-=======
-// import thunk from 'redux-thunk';
->>>>>>> undo
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import rootReducer from 'reducers/reducerIndex';
 import * as actions from 'actions/indexActions';
 
-const thunk = store => next => action =>
-  typeof action === 'function' ?
-    action(store.dispatch, store.getState) :
-    next(action)
-
 export default createStore(
   rootReducer,
-<<<<<<< HEAD
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk /* logger()*/),
-=======
-  applyMiddleware(thunk, /*logger()*/),
->>>>>>> undo
+  applyMiddleware(thunk /*, logger()*/)
 );
