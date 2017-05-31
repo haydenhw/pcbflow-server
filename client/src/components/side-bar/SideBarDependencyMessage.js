48f1cbc0243d2ff90ef1d16172911bec7c77e096
hayden7913 <hayden321@gmail.com>
2017-05-31 15:20:23 -0700

    fixed lint errors

--- client/src/components/side-bar/SideBarDependencyMessage.js
+++ client/src/components/side-bar/SideBarDependencyMessage.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './side-bar-styles/SideBarDependencyMessage.css';
 
 export default function SideBarDependencyMessage(props) {
+  const { moduleName, showAll } = props;
   return (
     <div className="dependency-message">
       <p>Showing dependencies for module</p>
-      <h2>{props.moduleName}</h2>
-      <p className="show-all" onClick={props.showAll}>Show all modules</p>
+      <h2>{moduleName}</h2>
+      <p className="show-all" onClick={showAll}>Show all modules</p>
     </div>
   );
 }
+
+SideBarDependencyMessage.propTypes = {
+  moduleName: PropTypes.string.isRequired,
+  showAll: PropTypes.func.isRequired,
+};
