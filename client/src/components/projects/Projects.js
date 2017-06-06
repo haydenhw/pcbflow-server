31f417ff426396aa17c8d14421b1fec700217d12
hayden7913 <hayden321@gmail.com>
2017-06-05 17:57:52 -0700

    modifying navbar styles

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -4,32 +4,35 @@ import Link from 'react-router';
 import * as actions from 'actions/indexActions';
 import store from 'reduxFiles/store';
 
-import NavDropdown from 'components/NavDropdown';
+import NavDropdown from 'components/NavDropdown'
+import TopNavbar from 'components/top-navbar/TopNavbar';
 import ProjectsList from './ProjectsList';
 import ProjectsAddButton from './ProjectsAddButton';
-import TopNavbar from 'components/top-navbar/TopNavbar';
- 
-// import './projects-styles/floatGrid.css';
-// import './projects-styles/Projects.css';
-import 'components/styles/NavDropdown.css'
+
+import 'components/top-navbar/top-navbar-styles/TopNavbar.css';
+import './projects-styles/floatGrid.css';
+import './projects-styles/Projects.css';
 
 export default function Projects(props) {
   return (
     <div>
-      <nav>
+      <div className="navWide project-navbar">
+        <div className="logo-link" onClick={props.handleLinkClick}>
+            <img className="logo-image" src="images/logo4.png" alt="" />
+            <span className="logo-text">PCB<span className="bold">flow</span></span>
+        </div>
         <NavDropdown />
-       {/* <div className="dropdown">
-          <ul id="nav">
-            <li><a href="#">DROPDOWN</a>
-                <ul>
-                    <li><a href="#">dropdown 1</a></li>
-                    <li><a href="#">dropdown 2</a></li>
-                    <li><a href="#">dropdown 3</a></li>
-                </ul>
-            </li>
-        </ul>
-       </div> */}
-    </nav>
+      </div>
+      <div className="button-bar">
+        <div className="row-project">
+          <div className="col-wrapper">
+            <div>
+              <ProjectsAddButton />
+            </div>
+          </div>
+        </div>
+      </div>
+      <ProjectsList />
     </div>
   );
 }
