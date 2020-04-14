d45e285eb2fda79e5223c69290a0617eed1c0522
hayden7913 <hayden321@gmail.com>
2020-04-14 16:57:10 -0700

    Rename article -> project

--- test/projects.fixtures.js
+++ test/projects.fixtures.js
@@ -69,27 +69,27 @@ function makeProjects() {
   return [project];
 }
 
-function makeMaliciousArticle() {
-  const maliciousArticle = {
+function makeMaliciousProject() {
+  const maliciousProject = {
     id: 911,
     style: 'How-to',
     date_published: new Date().toISOString(),
     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
     content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`
   }
-  const expectedArticle = {
-    ...maliciousArticle,
+  const expectedProject = {
+    ...maliciousProject,
     title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
     content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
   }
   return {
-    maliciousArticle,
-    expectedArticle,
+    maliciousProject,
+    expectedProject,
   }
 }
 
 module.exports = {
   makeProjects,
   makeProjectsWithModules,
-  makeMaliciousArticle,
+  makeMaliciousProject,
 }
