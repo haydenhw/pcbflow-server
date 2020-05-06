b8b3844e52066c91fd81e2ecb9f2b0cf7f0fa189
hayden7913 <hayden321@gmail.com>
2020-05-06 10:45:41 -0700

    Deletes unneeded files

--- client/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.static('build'));
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
-
--- client/test/projectActions.test.js
+++
@@ -1,6 +0,0 @@
-const res = true;
-console.log(res);
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(res).toBe(true);
-});
