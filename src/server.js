a049d2b635743622ee409a7d73910da606556e0f
hayden7913 <hayden321@gmail.com>
2020-09-18 16:02:13 -0700

    Changes default server port to 8082

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DB_URL } = require('./config')
 
+if (!DB_URL) {
+  throw new Error('DB_URL environment variable is undefined')
+}
+
 const db = knex({
   client: 'pg',
   connection: DB_URL,
