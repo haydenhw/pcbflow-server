4edfcf186c7fec5df18625cb7501b10b0947f2c8
hayden7913 <hayden321@gmail.com>
2020-09-24 22:45:28 -0700

    Adds README.md

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SRC=/Users/hayden/MEGA/projects/PCBflow/pcbflow-server/
+DEST=/home/ubuntu/projects/pcbflow-server/
+SSH="ssh -i ~/.ssh/MyKeyPair.pem ubuntu@${EC2IP4}"
+
+# create project folder and remove existing source files
+$SSH "mkdir -p ${DEST}"
+
+# scp project dir to ec2 projects dir
+rsync -av -progress -e "ssh -i ~/.ssh/MyKeyPair.pem" --exclude='/.git' --filter="dir-merge,- .gitignore" \
+ ${SRC} ubuntu@$EC2IP4:${DEST}
+
+# cd into project dir
+$SSH "cd ${DEST} && /bin/bash /bin/bash run.sh"
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run --name pcbflow -p 8082:8082 -d pcbflow-server:latest
+docker container stop pcbflow
+docker run --name pcbflow -p 8082:8082 --rm -d pcbflow-server:latest
